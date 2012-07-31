8642bc0d37422df12e9ebd1454d0664c486cba75
luxgengroup <ck@cklin.org>
2012-07-30 23:38:58 -0700

    add layout links(with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -57,5 +57,19 @@ describe PagesController do
     end
   end
   
-  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Help")
+    end
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
 end
